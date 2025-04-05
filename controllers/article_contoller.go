1ca1d6b66c2a8c2179751deae44e95ef9a16a053
Chuan81 <819937858@qq.com>
2025-04-05 22:41:52 +0800

    CreateArticle Init

---
+++ controllers/article_contoller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"exchangeapp/global"
+	"exchangeapp/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateArticle(ctx *gin.Context) {
+	var article models.Article
+
+	if err := ctx.ShouldBindJSON(&article); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := global.Db.AutoMigrate(&article); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := global.Db.Create(&article).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, article)
+}
