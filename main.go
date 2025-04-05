a739967dea99e3dea21d438ecadb4f47cfee5796
Chuan81 <819937858@qq.com>
2025-04-05 14:04:46 +0800

    gin app init

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"exchangeapp/config"
-	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
 
+	type Info struct {
+		Message string
+	}
+
+	InfoTest := Info{
+		Message: "pong",
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, InfoTest)
+	})
+	r.Run(config.AppConfig.App.Port)
 }
