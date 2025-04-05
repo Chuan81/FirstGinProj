25c309d1b6d22a15f558d0d2c751b8d12984382e
Chuan81 <819937858@qq.com>
2025-04-05 22:57:58 +0800

    article finish commit

--- controllers/article_contoller.go
+++ controllers/article_contoller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"errors"
 	"exchangeapp/global"
 	"exchangeapp/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func CreateArticle(ctx *gin.Context) {
@@ -38,4 +40,21 @@ func GetArticles(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, articles)
+}
+
+func GetArticlesByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var article models.Article
+
+	if err := global.Db.Where("id = ?", id).First(&article).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusOK, article)
 }
